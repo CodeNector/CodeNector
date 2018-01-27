c0cd9565b8549628c12f080f29b2e2b521d4a9e6
perez6736 <perez6736@gmail.com>
2018-01-27 13:55:41 -0500

    basic login component

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const Login = props => (
-    <form action="/login" method="post">
-    <div>
-        <label>Username:</label>
-        <input type="text" name="username"/>
-    </div>
-    <div>
-        <label>Password:</label>
-        <input type="password" name="password"/>
-    </div>
-    <div>
-        <input type="submit" value="Log In"/>
-    </div>
-</form>
-);
+class Login extends Component {
+    // Setting the initial values of this.state.username and this.state.password
+    state = {
+        username: "",
+        password: ""
+    };
+    
+    // handle any changes to the input fields
+    handleInputChange = event => {
+        // Pull the name and value properties off of the event.target (the element which triggered the event)
+        const { name, value } = event.target;
+
+        // Set the state for the appropriate input field
+        this.setState({
+        [name]: value
+        });
+    };
+
+    // here we need to send the username and password to the server.. so that passport can ddo stuff with that. 
+    handleFormSubmit = event => {
+        event.preventDefault();
+        alert(`Username: ${this.state.username}\nPassword: ${this.state.password}`);
+        this.setState({ username: "", password: "" });
+  };
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleInputChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleInputChange}
+        />
+        <button onClick={this.handleFormSubmit}>Submit</button>
+      </form>
+    );
+  }
+}
 
 export default Login;
