8ebaa64cc4c79e8f2cdfa11a143788e0c8d99702
perez6736 <perez6736@gmail.com>
2018-01-31 11:34:06 -0500

    setting up stuff for passport again

--- server.js
+++ server.js
@@ -11,11 +11,16 @@ const io = socketIO(server);
 // - Danny
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
 
 //connect to the db - Danny -
 mongoose.connect('mongodb://localhost/CodeNector');
 const db = mongoose.connection;
+
 
 
 // Serve up static assets (usually on heroku)
