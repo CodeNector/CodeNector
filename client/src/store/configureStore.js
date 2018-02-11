a3039cc040b789645afde54d50ae2dd12061ae5a
Ovieh <ovieh.mosley@gmail.com>
2018-02-11 09:41:00 -0500

    added redux-logger

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -3,6 +3,7 @@ import rootReducer from '../reducers';
 import ReduxPromise from 'redux-promise';
 import axios from 'axios';
 import axiosMiddleware from 'redux-axios-middleware';
+import logger from 'redux-logger';
 
 const client = axios.create({
 	baseURL: 'http://localhost:3001/api',
@@ -12,13 +13,13 @@ const client = axios.create({
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
 export default function configureStore(preloadedState) {
 	return createStore(
 		rootReducer,
 		preloadedState,
 		composeEnhancers(
 			applyMiddleware(
+				logger,
 				ReduxPromise,
 				axiosMiddleware(client)
 			)
