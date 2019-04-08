df70ec31a4832e1afcf2fbc7c6443aa18793dea2
ovieh <ovieh.mosley@gmail.com>
2019-04-08 15:19:10 +0100

    push build

---
+++ client/build/precache-manifest.64e489ce0f76d6435e3538b54592d34b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "03019bdb72b41a18f0b0bb9390d7c208",
+    "url": "/static/media/backgroundImage.03019bdb.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "60e4fbe5723700c7e6bb",
+    "url": "/static/js/main.2c2f1e3a.chunk.js"
+  },
+  {
+    "revision": "c9d636066f429febbd5b",
+    "url": "/static/js/2.3c7496cf.chunk.js"
+  },
+  {
+    "revision": "60e4fbe5723700c7e6bb",
+    "url": "/static/css/main.797a36ac.chunk.css"
+  },
+  {
+    "revision": "c9d636066f429febbd5b",
+    "url": "/static/css/2.e8bd7389.chunk.css"
+  },
+  {
+    "revision": "7698fbc7efd948d7803f1f31a2d4c613",
+    "url": "/index.html"
+  }
+];
